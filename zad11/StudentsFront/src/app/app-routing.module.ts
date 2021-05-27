3cd14c6ffb74eb1c2258800cceb00449834b4f14
PawelPytel <pawel.pytel@op.pl>
2021-05-27 19:57:49 +0200

    zad11

---
+++ zad11/StudentsFront/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentsComponent } from './students/students.component';
+
+
+const routes: Routes = [
+  { path: 'detail/:id', component: StudentDetailComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/students', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
